47c17d581a4a0e7e4ca21a1f5433ecdc9588cca8
N0tAdm1n <ahmed29shamir@gmail.com>
2026-08-04 20:34:33 +0530

    add object writeToDisk

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,38 @@
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
 import java.util.List;
+import java.util.zip.DeflaterOutputStream;
 
 public class Main {
     static void main(String[] args) {
         String command = args[0];
-
+        String filename = "";
+        if(args.length >= 2) {
+            if(args[1].equals("-w")) {
+                if(args.length > 2) filename = args[2];
+            } else {
+                filename = args[1];
+            }
+        }
         switch (command) {
             case "init" :
                 initializeRepository();
+                break;
+            case "hash-object" :
+                if(filename.isEmpty()) {
+                    System.out.println("please enter a file name");
+                    break;
+                }
+
+                writeToDisk(filename);
                 break;
             default:
                 System.out.println(command + "is not a valid command");
@@ -47,5 +68,67 @@ public class Main {
             System.out.println("Failed to initialize project");
             System.out.println(e.getMessage());
         }
+    }
+
+    public static void writeToDisk(String filename) {
+        byte[] blob = createBlob(filename);
+        String hexString = generateHexString(blob);
+
+        String folderName = hexString.substring(0, 2);
+        String fileName   = hexString.substring(2, 40);
+
+        Path objectPath = Paths.get(".minigit", "objects", folderName, fileName);
+
+        try {
+            Files.createDirectories(objectPath.getParent());
+
+            try(FileOutputStream fos = new FileOutputStream(objectPath.toFile());
+                DeflaterOutputStream dos = new DeflaterOutputStream(fos)) {
+
+                dos.write(blob);
+
+            }
+            System.out.println("Successfully wrote the object to" + objectPath);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static byte[] createBlob(String filename) {
+        Path filePath = Paths.get("src/" + filename);
+
+        byte[] blob = null;
+        try {
+            byte[] byteFile = Files.readAllBytes(filePath);
+
+            String filesize = String.valueOf(Files.size(filePath));
+            String header = "blob " + filesize + '\0';
+            byte[] headerBytes = header.getBytes(StandardCharsets.UTF_8);
+
+            blob = new byte[headerBytes.length + byteFile.length];
+
+            System.arraycopy(headerBytes, 0, blob, 0, headerBytes.length);
+            System.arraycopy(byteFile, 0, blob, headerBytes.length, byteFile.length);
+        } catch (IOException e) {
+            System.out.println("unable to read file " + e.getMessage());
+        }
+        return blob;
+    }
+
+    public static String generateHexString(byte[] blob) {
+        String hexString = null;
+
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            byte[] hashedBlob = digest.digest(blob);
+
+            hexString = HexFormat.of().formatHex(hashedBlob);
+            //System.out.println(hexString);
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return hexString;
     }
 }
