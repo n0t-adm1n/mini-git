7bb1b963d9c75eb46a789678f1f3d8aa7a7afd32
N0tAdm1n <ahmed29shamir@gmail.com>
2026-08-17 13:54:30 +0530

    ref: change branchCommand to show branches when no arguement is given

--- src/commands/BranchCommand.java
+++ src/commands/BranchCommand.java
@@ -6,10 +6,19 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class BranchCommand implements GitCommand{
     @Override
     public void execute(String[] args) {
+        // no arguments passed with the command
+        if(args.length == 1) {
+            listBranches();
+            return;
+        }
+
+
         if(args.length != 2) {
             System.out.println("Usage: branch <branch-name>");
             return;
@@ -36,5 +45,46 @@ public class BranchCommand implements GitCommand{
         } catch (IOException e) {
             System.out.println("error in creating branch " + branchName + ". " + e.getMessage());
         }
+    }
+
+
+    private void listBranches() {
+        Path headsPath = Paths.get(".minigit/refs/heads");
+
+        if(!Files.exists(headsPath)) return;
+
+        // finding the active branch
+        String activeBranch = null;
+
+        try {
+            Path headPath = Paths.get(".minigit/HEAD");
+            if(Files.exists(headPath)) {
+                String headContent = Files.readString(headPath).trim();
+                if(headContent.contains(("ref: refs/heads/"))) {
+                    activeBranch = headContent.substring(16);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading the HEAD file. " + e.getMessage());
+        }
+
+        // getting every branch name
+        try(Stream<Path> paths = Files.list(headsPath)) {
+            List<String> branches = paths
+                    .map(p -> p.getFileName().toString())
+                    .sorted()
+                    .toList();
+
+            for(String branch : branches) {
+                if(branch.equals(activeBranch)) {
+                    System.out.println("* " + branch);  // highlighting the active branch
+                } else {
+                    System.out.println("  " + branch);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading branch names. " + e.getMessage());
+        }
+
     }
 }
